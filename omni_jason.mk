031e62f12f7562e1d24a947319efd7623ba34a1a
dianlujitao <dianlujitao@lineageos.org>
2018-02-04 22:05:27 +0800

    jason: Update for 8.1
     * Update init script
     * Update QSEE blobs from R11sROM_11_OTA_2590_all_201801030346
     * Remove unused QSEE blobs

--- omni_jason.mk
+++ omni_jason.mk
@@ -22,10 +22,6 @@ $(call inherit-product, build/target/product/embedded.mk)
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
 
-# Time Zone data for recovery
-PRODUCT_COPY_FILES += \
-    bionic/libc/zoneinfo/tzdata:recovery/root/system/usr/share/zoneinfo/tzdata
-
 PRODUCT_PROPERTY_OVERRIDES += \
     sys.usb.controller=a800000.dwc3 \
     sys.usb.rndis.func.name=rndis_bam \
